d6bd293ffbb443d76e5d303cea2ad15dd6990b64
unknown <kasia.github@apsnet.pl>
2026-05-17 11:03:32 +0200

    nwm

--- src/main/java/edziekanat/isi/repositories/UserRepository.java
+++ src/main/java/edziekanat/isi/repositories/UserRepository.java
@@ -12,7 +12,7 @@ import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class UserRepository {
+public class    UserRepository {
     private final JdbcTemplate jdbcTemplate;
 
     public UserRepository(JdbcTemplate jdbcTemplate) {
